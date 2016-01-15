17c98d45ee8ff5939d09d4cb33cc3276e9987088
Kyler Conena <kylerownsx62@aim.com>
2016-01-14 19:14:47 -0500

    Updated damage backtrack
    Added the damage backtrack feature

--- game/scripts/vscripts/heroes/hero_faceless_void/time_walk.lua
+++ game/scripts/vscripts/heroes/hero_faceless_void/time_walk.lua
@@ -6,7 +6,7 @@ function TimeWalk( keys )
 	local caster_location = caster:GetAbsOrigin()
 	local target_point = keys.target_points[1]
 	local ability = keys.ability
-	local caster_aura = keys.caster_aura
+	local modifier = keys.modifier
 
 	-- Distance calculations
 	local speed = ability:GetLevelSpecialValueFor("speed", (ability:GetLevel() - 1))
@@ -21,7 +21,7 @@ function TimeWalk( keys )
 	ability.time_walk_traveled_distance = 0
 
 	-- Apply the slow aura and invlunerability modifier to the caster
-	ability:ApplyDataDrivenModifier(caster, caster, caster_aura, {duration = duration})
+	ability:ApplyDataDrivenModifier(caster, caster, modifier, {duration = duration})
 end
 
 --[[Author: Pizzalol
@@ -39,4 +39,66 @@ function TimeWalkMotion( keys )
 		-- Remove the motion controller once the distance has been traveled
 		caster:InterruptMotionControllers(false)
 	end
+end
+
+--[[Author: YOLOSPAGHETTI
+	Date: 14.01.2016
+	Creates an array of all taken damage within last 2 seconds]]
+function CalculateDamage( keys )
+	local damage_taken = keys.DamageTaken
+	local backtrack_time = keys.BacktrackTime
+	
+	-- Temporary damage array and index
+	local temp = {}
+	local temp_index = 0
+	
+	-- Global damage array and index
+	local caster_index = 0
+	if caster_damage == nil then
+		caster_damage = {}
+	end
+	
+	-- Sets the damage and game time values in the tempororary array, if caster was attacked within 2 seconds of current time
+	-- Even indexes are damage values, odd are game times
+	
+	while caster_damage do
+		if caster_damage[caster_index] == nil then
+		break
+		elseif Time() - caster_damage[caster_index+1] <= backtrack_time then
+			temp[temp_index] = caster_damage[caster_index]
+			temp[temp_index+1] = caster_damage[caster_index+1]
+			temp_index = temp_index + 2
+		end
+		caster_index = caster_index + 2
+	end
+	
+	-- Places most recent damage and current time in the temporary array
+	temp[temp_index] = damage_taken
+	temp[temp_index+1] = Time()
+	
+	-- Sets the global array as the temporary array
+	caster_damage = temp
+end
+
+--[[Author: YOLOSPAGHETTI
+	Date: 14.01.2016
+	Removes all damage taken within 2 seconds of cast]]
+function RemoveDamage ( keys )
+	local caster = keys.caster
+	local backtrack_time = keys.BacktrackTime
+	local damage_sum = 0
+	local caster_index = 0
+	
+	-- Sums damage over the last 2 seconds
+	while caster_damage do
+		if caster_damage[caster_index] == nil then
+		break
+		elseif Time() - caster_damage[caster_index+1] <= backtrack_time then
+			damage_sum = damage_sum + caster_damage[caster_index]
+		end
+		caster_index = caster_index + 2
+	end
+	
+	-- Adds damage to caster's current health
+	caster:SetHealth(caster:GetHealth() + damage_sum)
 end
